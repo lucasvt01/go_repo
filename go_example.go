57937128dfb20243d5a838cb5e3c6554d839bb09
lucasvt01 <lucasvt01@gmail.com>
2023-05-13 16:04:22 -0400

    initial commit

---
+++ go_example.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Conta struct {
+	saldo float64
+}
+
+func (c *Conta) depositarDezReais() float64 {
+	return c.saldo + 10
+}
+
+func main() {
+	contaTeste := Conta{saldo: 10}
+
+	contaTeste.depositarDezReais()
+	contaTeste.depositarDezReais()
+
+	fmt.Println(contaTeste)
+}
